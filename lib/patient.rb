f9c450524c8a00beeebb3c9e3906bbe811458b82
Bry Cox <carl.cc@gmail.com>
2019-03-08 08:39:14 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,12 +1,12 @@
 class Patient
 
-  attr_accessor :name, :appointments
+  attr_accessor :name
 
   @@all = []
 
   def initialize(name)
     @name = name
-    @@all << self
+    self.class.all << self
   end
 
   def new_appointment(doctor, date)
