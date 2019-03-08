46958cc5aa4626160efaf65bec5ff9fccb8089fc
Bry Cox <carl.cc@gmail.com>
2019-03-08 08:59:16 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -9,14 +9,6 @@ class Patient
     self.class.all << self
   end
 
-  def new_appointment(doctor, date)
-    Appointment.new(date, self, doctor)
-  end
-
-  def appointments
-    appointment.all.select {|appointment| appointment.patient == self}
-  end
-
   def self.all
     @@all
   end
